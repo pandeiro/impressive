91eafe825cba602ed5b096cd11a64167b4f2f22d
Murph <pandeiro@gmail.com>
2012-10-25 09:44:05 -0200

    redefine step to allow for arbitrary attributes

--- src/impressive/core.clj
+++ src/impressive/core.clj
@@ -1,6 +1,7 @@
 (ns impressive.core
   (:use [hiccup.core :only [html]]
-        [hiccup.page :only [html5]]))
+        [hiccup.page :only [html5]])
+  (:require [clojure.set :refer [rename-keys]]))
 
 (defn impress
   "Creates an HTML5 skeleton for an impress.js project, assuming the project
@@ -27,27 +28,20 @@ uses the following structure:
     [:script "impress().init();"]]))
 
 (defn step
-  "Returns an impress.js slide (<div class=\"step\">) in html. Arguments: a string or
-keyword id (optional), a map with :x, :y and optional :z, :scale, :rotate, and
-:class attributes, and the slide content as hiccup vectors.
-
-Example:
-(step :slide-id {:x 1000 :y 0 :rotate 180 :scale 2 :class [\"slide\" \"dark\"]}
-      [:h1 \"This is my slide\"])"
   [& args]
-  (let [first-arg                          (first args)
-        has-id?                            (or (string? first-arg) (keyword? first-arg))
-        id                                 (if has-id? (name first-arg))
-        {:keys [x y z scale rotate class]} (if has-id? (second args) first-arg)
-        class                              (if (string? class) (vector class) class)
-        classes                            (apply str (interpose " " (list* "step" class)))
-        attrs                              (into {} (filter
-                                                     #(not (nil? (val %)))
-                                                     {:id id, :class classes, :data-x x
-                                                      :data-y y, :data-z z, :data-scale scale
-                                                      :data-rotate rotate}))
-        content                            (nthnext args (or (and has-id? 2) 1))]
-
+  (let [first-arg  (first args)
+        has-id?    (or (string? first-arg) (keyword? first-arg))
+        id         (if has-id? (name first-arg))
+        opts-map   (if has-id? (second args) first-arg)
+        class      (if (string? (:class opts-map))
+                     (vector (:class opts-map))
+                     (:class opt-map))
+        classes    (apply str (interpose " " (list* "step" class)))
+        attrs      (rename-keys opts-map {:x :data-x, :y :data-y,
+                                          :z :data-z, :scale :data-scale,
+                                          :rotate :data-rotate})
+        attrs      (remove nil? (merge attrs {:id id :class classes}))
+        content    (nthnext args (or (and has-id? 2) 1))]
     (html
      [:div attrs content])))
 
